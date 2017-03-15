e63c8cdba0959fe0877e076ddd59aaa38c53df7b
fahim chowdhury <fahim@gmx.com>
2017-03-15 10:35:27 +0000

    Move template string to the template file and added npm installation inside the init task.

--- init.js
+++ init.js
@@ -1,26 +1,48 @@
 const ComponentUtil = require('./utils/componentUtil.js');
+const TEMPLATE = require('./template/component.js');
+const CLI = require('./utils/cli.js');
 const fs = require('fs');
 const path = require('path');
 
 const VCCPInit = {
-    APP_TEMPLATE: '<div id="app"><router-view></router-view></div>',
     init() {
+        this.readPackageJSON();
+        this.install();
         this.createApp();
         ComponentUtil.cleanUp('../src/App.vue');
         ComponentUtil.cleanUp('../src/components/Hello.vue');
         this.updateFiles();
     },
-    createApp() {
-        ComponentUtil.createComponent('app', this.APP_TEMPLATE);
+    install() {
+        CLI.execute('npm install --save vue-style-loader && npm install --save css-loader && npm install readline --save', process.cwd());
     },
+    readPackageJSON() {
+        let packagePath = path.resolve(process.cwd() + '/package.json');
 
+            fs.readFile(packagePath, 'utf8',
+            function(err, data) {
+                if (err) throw err;
+                let json = JSON.parse(data);
+                json = this.updatePackage(json);
+                fs.writeFile(packagePath, JSON.stringify(json,null,2), 'utf8', function(err) {
+                    if (err) return console.log(err);
+                });
+            }.bind(this));
+    },
+    updatePackage(json) {
+        json['scripts']['create-component'] = "node vue-component-creator/create-component.js";
+        return json;
+    },
+    createApp() {
+        ComponentUtil.createComponent('app', TEMPLATE.appHTML);
+    },
     updateFiles() {
-      ComponentUtil.updateFile( '../src/main.js', ['./App'],  './components/app/app');
-      ComponentUtil.updateFile( '../src/router/index.js', [
-        'import Hello from \'@/components/Hello\'',
-        'component: Hello',
-        'name: \'Hello\',',
-        ','
-      ],  '');
+        ComponentUtil.updateFile('../src/main.js', ['./App'], './components/app/app');
+        ComponentUtil.updateFile('../src/router/index.js', [
+            'import Hello from \'@/components/Hello\'',
+            'component: Hello',
+            'name: \'Hello\',',
+            ','
+        ], '');
     }
 }.init();
--- template/component.js
+++ template/component.js
@@ -1,5 +1,6 @@
 module.exports = {
   html: '<div></div>',
   vue: '<template src="./template/template.html">\n</template><script src="./src/component.js">\n</script><style lang="scss" scoped src="./style/style.scss"></style>',
-  js: 'export default {\nname: \'{componentName}\'\n};'
+  js: 'export default {\nname: \'{componentName}\'\n};',
+  appHTML: '<div id="app"><router-view></router-view></div>'
 };
